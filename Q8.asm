49ce3b814271429c435eff83770ab6db0488508f
reynol-31 <reynol.23cs119@sode-edu.in>
2025-05-18 14:34:21 +0530

    Delete Q8.asm

--- Q8.asm
+++
@@ -1,9 +0,0 @@
-AREA Q8, CODE, READONLY
-ENTRY
-	LDR r5, [r1]       ; Load value from memory address in r1 (0x40000000) → r5 = 10
-	LDR r6, [r1, #4]   ; Load value from (r1 + 4) = (0x40000000 + 4) → r6 = 20
-	LDR r7, [r1, #8]   ; Load value from (r1 + 8) = (0x40000000 + 8) → r7 = 30
-
-	ADD r2, r5, r6     ; r2 = r5 + r6 = 10 + 20 = 30
-	SUB r2, r2, r7     ; r2 = r2 - r7 = 30 - 30 = 0
-	END
